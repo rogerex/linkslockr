c485fd0579cd0eb21e849834cb2cc99c40af0715
Roger Limachi <roger.limachi@incontact.com>
2017-09-20 12:27:05 -0400

    Correct installation

--- menu-init.js
+++ menu-init.js
@@ -1,8 +1,3 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// The onClicked callback function.
 function onClickHandler(info, tab) {
   if (info.menuItemId == "unlockerLink") {
     console.log(info);
@@ -15,7 +10,8 @@ chrome.runtime.onInstalled.addListener(function() {
   var title = "No Usar - Le Puya linque";
   var id = chrome.contextMenus.create({
 	  "title": title, 
-	  "contexts":"link",
-      "id": "unlockerLink"});
+	  "contexts": ["link"],
+      "id": "unlockerLink"}
+  );
   console.log("Decrypt Item created:" + id);
 });
