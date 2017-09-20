2c34d0c967b9781bd82bd30fd4f82a93b221c939
Roger Limachi <roger.limachi@incontact.com>
2017-09-20 13:07:26 -0400

    Basic behavior for safelink links

--- menu-init.js
+++ menu-init.js
@@ -1,6 +1,26 @@
 function onClickHandler(info, tab) {
   if (info.menuItemId == "unlockerLink") {
     console.log(info);
+	if (info.linkUrl.includes("http://puya.si/enc")) {
+		chrome.tabs.create({ url: info.linkUrl });
+	} else if (info.linkUrl.includes("http://safelinking.net/")) {
+		var hash = info.linkUrl.replace("http://safelinking.net/", "");
+		var params = "hash=" + hash;
+		
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", "http://safelinking.net/v1/protected", true);
+		xhr.setRequestHeader("Content-Type", "application/json");
+		xhr.onreadystatechange = function() {
+		  if (xhr.readyState == 4) {
+			var response = JSON.parse(xhr.responseText);
+			var decryptedURL = response.links[0].url;
+			chrome.tabs.create({ url: decryptedURL });
+		  }
+		}
+		xhr.send(JSON.stringify({ hash: hash }));
+    } else {
+	  alert("This is not a valid Link.");
+	}
   }
 };
 
@@ -9,9 +29,9 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 chrome.runtime.onInstalled.addListener(function() {
   var title = "No Usar - Le Puya linque";
   var id = chrome.contextMenus.create({
-	  "title": title, 
-	  "contexts": ["link"],
-      "id": "unlockerLink"}
-  );
+	"title": title, 
+	"contexts": ["link"],
+    "id": "unlockerLink"
+  });
   console.log("Decrypt Item created:" + id);
 });
